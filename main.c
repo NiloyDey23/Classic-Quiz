7e3fb09896e79c64e0ddc32f418c3d0b13da874c
NiloyDey23 <niloydey23@gmail.com>
2020-02-04 13:17:25 +0600

    Quiz Muiz

---
+++ main.c
@@ -0,0 +1,798 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define pinc 12345
+
+void edit_score();
+void show_score();
+
+
+int main()
+{
+
+    int pin;
+
+    printf("\n\n\t\t\tEnter pin for continue: ");
+    scanf("%d", &pin);
+
+    if(pin==pinc)
+    {
+            char ch[20];
+    int countr=0;
+    int i,j;
+        printf("\n\t\t\tpin matched!!!");
+
+                for(i=0;i<=20000;i++)
+                    {
+                        for(j=0;j<=40000;j++)
+                        {
+                        }
+                     }
+        system("cls");
+
+
+
+printf("\n\n**************************WELCOME TO OUR GAME************************************");
+printf("\n\n\t\t\t\tQUIZ  MUIZ\n\n");
+for(i=0;i<=20000;i++)
+                    {
+                        for(j=0;j<=20000;j++)
+                        {
+                        }
+                     }
+printf("\t\t\t\t\tcreated by CYANIDE");
+
+                   for(i=0;i<=40000;i++)
+                    {
+                        for(j=0;j<=30000;j++)
+                        {
+                        }
+                    }
+
+system("cls");
+
+
+printf("\n\n\n\t\tHow to play this game?\n\n");                                       //* Sadia Afrin 171-15-9502
+printf("\n\n\t1.There will be questions with four options. A. B. C. & D.\n");
+printf("\n\t    2.You have to choose one correct answer\n");
+printf("\n\t       3.If your answer is correct you will get points.\n");
+printf("\n\t          4.press enter after each correct answer for the next question\n");
+printf("\n\t             5.If your answer is wrong, the game will be end.\n");
+printf("\n\n\n\n\t\t\tPress any key to continue");
+
+getch();
+
+putch('\a');
+
+printf(".");
+for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=10000;j++)
+    {
+
+    }
+}
+printf(".");
+for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=15000;j++)
+    {
+
+    }
+}
+printf(".");
+
+for(i=0;i<=10000;i++)
+{
+    for(j=0;j<=30000;j++)
+    {
+
+    }
+}
+system("cls");
+
+    printf("\n\n\t\tEnter your name:  ");
+    scanf("%s", ch);
+
+
+    system("cls");
+
+    printf("\n\n\n\t\t\t\tAre you ready %s?????\n\n\n", ch);
+
+    for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=15000;j++)
+    {
+
+    }
+}
+
+printf("-------------------------------------------------------------------------------------------\n");
+
+     for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=20000;j++)
+    {
+
+    }
+}
+
+printf("--------------------------------------------------------------------------------------------\n");
+
+
+
+
+
+for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=10000;j++)
+    {
+
+    }
+}
+
+ printf("press any key to continue");
+
+ putch('\a');
+
+ getch();
+
+ for(i=0;i<=10000;i++)
+{
+    for(j=0;j<=20000;j++)
+    {
+
+    }
+}
+
+system("cls");
+
+printf("\n\n\n\n\tLoading");
+
+ for(i=0;i<=10000;i++)
+{
+    for(j=0;j<=20000;j++)
+    {
+
+    }
+}
+printf("||||");
+
+ for(i=0;i<=25000;i++)
+{
+    for(j=0;j<=10000;j++)
+    {
+
+    }
+}
+printf("|||||||||");
+
+ for(i=0;i<=10000;i++)
+{
+    for(j=0;j<=20000;j++)
+    {
+
+    }
+}
+printf("||||");
+
+ for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=20000;j++)
+    {
+
+    }
+}
+printf("|||||||||||");
+
+ for(i=0;i<=15000;i++)
+{
+    for(j=0;j<=20000;j++)
+    {
+
+    }
+}
+printf("||||||");
+
+
+ for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=30000;j++)
+    {
+
+    }
+}
+printf("|||||");
+
+ for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=10000;j++)
+    {
+
+    }
+}
+printf("||||||||");
+
+ for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=30000;j++)
+    {
+
+    }
+}
+printf("|||||");
+
+putch('\a');
+
+    system("cls");
+
+    printf("\n\n\t\tWhich is the following is the largest Muslim country?");                 //Jahid Hasan 171-15-9439
+		printf("\n\n\t\tA.Bangladesh\t\tB.Indonesia\n\n\t\tC.Malaysia\t\tD.Iraq");
+		if (toupper(getch())=='B')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\n\t\tyou,ve got 1 point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is B.Indonesia");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\nWhat is the National Game of England?");
+		printf("\n\nA.Football\t\tB.Basketball\n\nC.Cricket\t\tD.Baseball");
+		if (toupper(getch())=='C')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is C.Cricket");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\n\t\tBadminton is the national sport of-");
+		printf("\n\n\t\tA.Malaysia\t\tB.Scotland\n\n\t\tC.China\t\t\tD.Japan");
+		if (toupper(getch())=='A')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is A.Malaysia");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\nWhich one is the oldest civilization?");
+		printf("\n\nA.Egyptian\t\tB.Indus\n\n C.Chinese\t\tD.Greek");
+		if (toupper(getch())=='A')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.Egyptian");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\n\t\tWhich country is not a member of BIMSTEC?");
+		printf("\n\n\t\tA.India\t\tB.Bangladesh\n\n\t\tC.China\t\tD.Sri Lanka");
+		if (toupper(getch())=='C')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is C.China");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\nHow many members are in the UN security council?");
+		printf("\n\nA.10\t\tB.11\n\n C.12\t\tD.15");
+		if (toupper(getch())=='D')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is D.15");
+		       getch();
+		       goto score;
+
+		       }
+
+
+		system("cls");
+
+        printf("\n\n\t\tWho is the famous painter of Guernica?");
+		printf("\n\n\t\tA.Van Hogh\t\tB.Zainul Abedin\n\n\t\tC.Sahabuddin\t\tD.Picasso");
+		if (toupper(getch())=='D')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is D.Picasso");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\nWhich country has the world's tallest buiding?");
+		printf("\n\nA.USA\t\tB.US\n\n C.Africa\t\tD.Dubai");
+		if (toupper(getch())=='D')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is D.Dubai");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+
+		       printf("\n\n\t\tAndaman island belongs to which country?");
+		printf("\n\nA.India\t\tB.Bangladesh\n\n\t\tC.Burma\t\tD.Nepal");
+		if (toupper(getch())=='A')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+			 }
+
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is A.India");
+		       getch();
+		       goto score;
+
+		       }
+
+
+		system("cls");
+
+		       printf("\n\nWhich one is an island nation in the Mediterranean Ocean?");
+		printf("\n\nA.Gibraltar\t\tB.Tnisia\n\n C.Malta\t\tD.Albania");
+		if (toupper(getch())=='C')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is C.Malta");
+		       getch();
+		       goto score;
+
+		       }
+
+
+		system("cls");
+
+        printf("\n\n\t\tWhere is Louvre Museum?");
+		printf("\n\n\t\tA.Italy\t\tB.France\n\n\t\tC.Greece\t\tD.Switzerland");
+		if (toupper(getch())=='B')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is B.France");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+
+        printf("\n\nFirst Atom Bomb was dropped on-");
+		printf("\n\nA.Germany\t\tB.Australia\n\n C.Japan\t\tD.Italy");
+		if (toupper(getch())=='C')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is C.Cricket");
+		       getch();
+		       goto score;
+
+		       }
+
+
+		system("cls");
+
+
+        printf("\n\n\t\tWhere is the head quarter of Interpol?");
+		printf("\n\nA.Leo\t\tB.London\n\n\t\tC.Rome\t\tD.Paris");
+		if (toupper(getch())=='A')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is A.Leo");
+		       getch();
+		       goto score;
+
+		       }
+
+
+		system("cls");
+
+        printf("\n\nCentral Treaty Organization-");
+		printf("\n\nA.CTO\t\tB.CENTO\n\n C.CTRO\t\tD.CNTO");
+		if (toupper(getch())=='B')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is B.CENTO");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\n\t\tWhich contry won the World Cup Football Trophy five times?");
+		printf("\n\nA.Brazil\t\tB.Argentina\n\n\t\tC.Chilli\t\tD.West Germany");
+		if (toupper(getch())=='A')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got another point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is A.Brazil");
+		       getch();
+		       goto score;
+
+		       }
+
+
+		system("cls");
+
+
+		       printf("\n\n\t\tWhich one is not a Scandinavian State?");
+		printf("\n\n\t\tA.Sweden\t\tB.Holland\n\n\t\tC.Finland\t\tD.Norway");
+		if (toupper(getch())=='B')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou,ve got 1 point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is B.Holland");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\n\t\tWhat is the name of the Golf Championship Trophy?");
+		printf("\n\n\t\tA.Zarja Cup\t\tB.Raider Cup\n\n\t\tC.Tomas Cup\t\tD.Davis Cup");
+		if (toupper(getch())=='B')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou,ve got 1 point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is B.Raider Cup");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\n\t\tThe people of which region are the happiest in the world?");
+		printf("\n\n\t\tA.Indian Sub-continent\t\tB.Gulf region\n\n\t\tC.Caribbean region\t\tD.Scandinavia");
+		if (toupper(getch())=='C')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou've got 1 point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is C.Caribbean region");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+        printf("\n\n\t\tThe most expensive city in the world-");
+		printf("\n\n\t\tA.New York\t\tB.Singapure\n\n\t\tC.Tokyo\t\tD.London");
+		if (toupper(getch())=='B')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou,ve got 1 point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is B.Singapure");
+		       getch();
+		       goto score;
+
+		       }
+
+		system("cls");
+
+
+        printf("\n\n\t\tWho is the richest persons in the world?");
+		printf("\n\n\t\tA.Bill Gates\t\tB.Carlos Slim\n\n\t\tC.Mukesh Ambani\t\tD.None of them");
+		if (toupper(getch())=='A')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou,ve got 1 point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is A.Bill Gates");
+		       getch();
+		       goto score;
+
+		       }
+
+
+		system("cls");
+
+        printf("\n\n\t\tWhich is the smallest country in the asia by population?");
+		printf("\n\n\t\tA.Bangladesh\t\tB.Maldives\n\n\t\tC.Malaysia\t\tD.Thailand");
+		if (toupper(getch())=='B')
+			{
+            printf("\n\n\t\tCorrect!!!");
+			countr++;
+			printf("\n\nyou,ve got 1 point\n\n\n\n ");
+			putch('\a');
+			getch();
+
+
+
+			 }
+		else
+		       {printf("\n\n\t\tWrong!!! The correct answer is B.Maldives");
+		       getch();
+		       goto score;
+
+		       }
+
+
+		system("cls");
+
+
+
+		       score:
+
+		           printf("\n\n\n\t\t congrats, your total score is %d", countr);
+
+		           edit_score(countr, ch);
+
+		           printf("\n\n\t\tpress any key to view the highest score:\n\n\n");
+
+		           getch();
+
+		           show_score();
+
+
+
+    return 0;
+    }
+
+    else
+    {
+        int i,j;
+        putch('\a');
+        printf("\t\t\tWrong Pincode!!!\n");
+
+        for(i=0;i<=20000;i++)
+            {
+                for(j=0;j<=20000;j++)
+                {
+
+                }
+            }
+
+        printf("\n\n\t\t\tExiting");
+
+        for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=20000;j++)
+    {
+
+
+    }
+}
+printf(".");
+        for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=20000;j++)
+    {
+
+
+    }
+}
+printf(".");
+        for(i=0;i<=20000;i++)
+{
+    for(j=0;j<=20000;j++)
+    {
+
+
+    }
+}
+printf(".");
+    }
+
+
+}
+
+
+void edit_score(int score, char name[20])                      // Niloy Dey 171-15-9473
+	{
+
+	int scr=0;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s %d",&nm,&scr);
+	if (score>=scr)
+	  {
+        int sc;
+        sc=score;
+	    fclose(f);
+	    f=fopen("score.txt","w");
+	    fprintf(f,"%s\n%d",name,sc);
+	    fclose(f);
+    }
+    }
+
+void show_score()                                    // Kaniz Fatema 171-15-9583
+    {                                                       //Shourov Kumar 171-15-9488
+
+        system("cls");
+
+	char n[20];
+	int scr;
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s %d",&n,&scr);
+
+
+	printf("\n\n\t\t_______________________________________________________");
+	printf("\n\n\n\t\t\t %s scored %d",n,scr);
+	printf("\n\n\t\t_______________________________________________________");
+	putch('\a');
+
+	fclose(f);
+	getch();
+
+
+	}
